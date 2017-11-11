02e50924752daad9e962a227d4f5e4c2662f069c
Alexander Satov <aleksandr-satov@mail.ru>
2017-11-11 20:01:10 +0500

    task 2 complete

---
+++ Task2/Task2/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task2.Models
+{
+    public class Student
+    {
+        [Required]
+        public int Id       { get; set; }
+        public int Mark     { get; set; }
+        public string Name  { get; set; }
+        public string Email { get; set; }
+    }
+}
