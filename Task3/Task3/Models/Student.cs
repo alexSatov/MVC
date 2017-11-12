c0d3f4ba0d3eb51ed5286cea75c63387b3d09553
Alexander Satov <aleksandr-satov@mail.ru>
2017-11-13 02:59:14 +0500

    task3 done

---
+++ Task3/Task3/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task3.Models
+{
+    public class Student
+    {
+        [Required]
+        [Range(1, 100)]
+        public int Id       { get; set; }
+
+        [Range(0, 100)]
+        public int? Mark    { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(20)]
+        public string Name  { get; set; }
+
+        [MaxLength(30)]
+        public string Email { get; set; }
+    }
+}
